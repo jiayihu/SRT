7310a27800f74f039aa7bdce18f02057ee27ce2c
Jiayi Hu <jiayi.ghu@gmail.com>
2019-12-27 15:11:30 +0100

    Finish first draft of FPS analysis

--- src/request_buffer_parameters.ads
+++ src/request_buffer_parameters.ads
@@ -1,7 +1,7 @@
 with System;
 package Request_Buffer_Parameters is
 
-   Request_Buffer_Priority : constant System.Priority := 7;
+   Request_Buffer_Priority : constant System.Priority := 9;
    --  proper analysis will determine the appropriate size of the request
    --  buffer
    Request_Buffer_Range : constant Positive := 5;
