cf28c47ec9a7af318fb6cb4b81df149db0f07c56
Jiayi Hu <jiayi.ghu@gmail.com>
2020-02-12 22:55:27 +0100

    Delete old overrun detection

--- src/fps/overrun.ads
+++
@@ -1,9 +0,0 @@
-with Ada.Real_Time;
-with Ada.Execution_Time;
-
-package Overrun is
-   type Limits_Array is array (0 .. 2) of Ada.Execution_Time.CPU_Time;
-
-   procedure Start (Index : Natural; Budget : Ada.Real_Time.Time_Span);
-   procedure Check (Index : Natural);
-end Overrun;
