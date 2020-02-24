775aaf287b13c01d0186c2d85f42ba49e15bd183
Alessio Gobbo <alexzx93@gmail.com>
2020-02-24 13:16:17 +0100

    Notify_Deadline_Miss enabled to print Task's name

--- src/fps/deadline_miss.adb
+++ src/fps/deadline_miss.adb
@@ -1,12 +1,18 @@
 with Ada.Real_Time; use Ada.Real_Time;
+with Ada.Text_IO; use Ada.Text_IO;
 
 package body Deadline_Miss is
    Deadline_Events : Deadline_Events_Array;
 
    protected body Handler is
-      procedure Notify_Deadline_Miss (Event : in out Timing_Event) is
+      procedure Notify_Deadline_Miss (Event : in out Timing_Event) is   
       begin
-         raise Program_Error with "Detected deadline miss";
+         --raise Program_Error with "Detected deadline miss";
+         for I in Task_Name loop
+            if Time_Of_Event(Deadline_Events(I)) = Time_Of_Event(Event) then
+               Ada.Text_IO.Put_Line ("Deadline Miss Detected - " & I'Image );
+            end if;
+         end loop;
       end Notify_Deadline_Miss;
    end Handler;
 
