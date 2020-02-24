7bea403e48f8f6e637d276caf683c47c7de10d9d
Alessio Gobbo <alexzx93@gmail.com>
2020-02-24 13:12:53 +0100

    release logic of OCP changed (T=3)

--- src/fps/auxiliary.ads
+++ src/fps/auxiliary.ads
@@ -1,5 +1,5 @@
 package Auxiliary is
-   type Range_Counter is mod 5;
+   type Range_Counter is mod 3;
    function Due_Activation (Param : Range_Counter) return Boolean;
    type Run_Counter is mod 1_000;
    Factor : constant Natural := 3;
